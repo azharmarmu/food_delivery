630efb0b5c0225c34220a25b51524707a54902e9
Azhar <azharuddin.koya@kotak.com>
2021-12-14 21:29:59 +0530

    Bottom navigation UI/Ux

---
+++ food_delivery/lib/ui/home_page.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _currentIndex = 3;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        selectedItemColor: Colors.orange,
+        unselectedItemColor: Colors.blueGrey,
+        onTap: (int index) {
+          _currentIndex = index;
+          setState(() {});
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_filled),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.message),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: '',
+          ),
+          // BottomNavigationBarItem(
+          //   icon: SvgPicture.asset(
+          //     'assets/icons/icon-user-line.svg',
+          //   ),
+          //   label: '',
+          // ),
+        ],
+      ),
+    );
+  }
+}
